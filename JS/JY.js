ceb5be2f0a62c413c9dcf87e597641e41b55537c
aditya bhat <adityabhat@adis-MacBook-Air.local>
2024-06-27 12:03:42 +0530

    Edu Changes

---
+++ JS/JY.js
@@ -0,0 +1,46 @@
+import { db, doc, setDoc } from './config.js';
+
+document.getElementById("submit").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const school = document.getElementById("school").value;
+    const curriculum = document.getElementById("curriculum").value;
+    const allSubjects = document.querySelectorAll("#subjectsTableBody tr");
+
+    let subjects = [];
+    let semester1 = [];
+    let semester2 = [];
+
+    allSubjects.forEach((row) => {
+        const subject = row.querySelector('input[id="subjects"]').value;
+        const s1g = row.querySelector('input[id="s1g"]').value;
+        const s2g = row.querySelector('input[id="s2g"]').value;
+
+        subjects.push(subject);
+        semester1.push(s1g);
+        semester2.push(s2g);
+    });
+
+    const file1 = document.getElementById("file1").value;
+    const file2 = document.getElementById("file2").value;
+
+    // const userEmail = document.cookie.split("sanitizedEmail=")[1].split(";")[0];
+    const userEmail = 'adirbhat@gmail.com';
+    // Create a document reference
+    const jyDocRef = doc(db, `students/${userEmail}/JY/${school}`);
+
+    // Set the document in Firestore
+    setDoc(jyDocRef, {
+        School: school,
+        Curriculum: curriculum,
+        Subjects: subjects,
+        Semester1: semester1,
+        Semester2: semester2,
+        file1: file1,
+        file2: file2,
+    }).then(() => {
+        alert("Data sent successfully");
+    }).catch((error) => {
+        alert("Error: " + error.message);
+    });
+}); 
---
+++ JS/config.js
@@ -0,0 +1,33 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDoc,
+    setDoc,
+    doc,
+    updateDoc,
+    query,
+    where,
+    getDocs,
+  } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAFqDKIWaADj5RpcO-p8Ny0bx2ZfFV8UsI",
+    authDomain: "college-cohesive-admin.firebaseapp.com",
+    projectId: "college-cohesive-admin",
+    storageBucket: "college-cohesive-admin.appspot.com",
+    messagingSenderId: "23786372825",
+    appId: "1:23786372825:web:9579d28db485315f2288c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db, collection, addDoc, getDoc, setDoc, doc, updateDoc, query, where, getDocs };
